327cd0f14a5e6d49a68b1d7458d244337b5488ad
nandan-19 <nandanks058@gmail.com>
2025-02-01 20:11:22 +0530

    commented down the sample example code in page.txs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import { useSession } from "next-auth/react";
-import { auth } from "@/lib/auth"
-
 export default  async function Home() {
-  const session = await auth()
- 
-  if (!session?.user) return null
-    
-    console.log(session.user.name);
-    console.log(session.user.email);
-    console.log(session.user.id);
-    console.log(session.user.image);
-    
+    <div></div>
 }
+
+// import { auth } from "@/lib/auth"
+// const session = await auth()
+// if (!session?.user) return null
+
+//   console.log(session.user.name);
+//   console.log(session.user.email);
+  
