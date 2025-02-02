c531f4fc1c0cd5d33b2fb7cff3db2c59aa433307
Nikhil002531 <nikhil9532007@gmail.com>
2025-02-02 08:32:59 +0530

    Created ButtonComponent.tsx

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface ButtonProps{
+  ButtonName: string;
+  TextColor:string;
+  ButtonColor1: string;
+  ButtonColor2: string;
+  className?: string;
+}
+
+const ButtonComponent = ({ButtonName,ButtonColor1, ButtonColor2, TextColor, className=""}:ButtonProps) => {
+  return (
+    <button type="button" className={`w-full ${ButtonColor1} ${TextColor} py-2 px-4 rounded-lg ${ButtonColor2} transition-all duration-300 ${className}`}>
+      {ButtonName}
+    </button>
+  );
+};
+
+export default ButtonComponent;
