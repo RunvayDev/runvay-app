0bee4e27eb0a39c9ffb73431e0c5a5b7e56ea14d
Raunak Rose <raunakrose15@gmail.com>
2025-02-12 16:46:02 +0530

    few api change

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import connectMongo from "@/lib/mongoose";
 import Order from "@/models/Order";
- import { auth } from "@/lib/auth";
+import { auth } from "@/lib/auth";
 import User from "@/models/User";
 
 export async function GET() {
@@ -9,19 +9,19 @@ export async function GET() {
     await connectMongo();
 
     const session = await auth();
-  
+
     if (!session) {
+      console.log("no session")
       return NextResponse.json(
         { message: "Invalid or expired session" },
         { status: 401 },
       );
     }
-    // console.log(session.userId);
-    const user = await User.findById(session?.user?.email);
+    const user = await User.findOne({ email: session?.user?.email });
     if (!user) {
       return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
-    // console.log(user);
+    console.log(user);
 
     const orders = await Order.find({ user: user._id })
       .populate("user", "name email")
@@ -32,7 +32,7 @@ export async function GET() {
     return NextResponse.json(orders);
   } catch (err) {
     if (err instanceof Error) {
-      // console.error("Order fetching error:", err);
+      console.error("Order fetching error:", err);
       return NextResponse.json(
         { message: "Failed to fetch Orders" },
         { status: 500 },
