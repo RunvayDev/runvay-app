328ee7efe2636cd301b608226cca57b664c44eef
nandan-19 <nandanks058@gmail.com>
2025-05-07 16:30:54 +0530

    added PKCE option in auth.ts file to fix the cookies issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -98,7 +98,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/signin",
   },
-
+  cookies: {
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
